5bca96e749f6fc890e16663d3ced540880c67120
alexy4744 <alexy4744@gmail.com>
2019-02-18 15:10:04 -0500

    add telophase

--- src/views/Anaphase.vue
+++ src/views/Anaphase.vue
@@ -6,12 +6,53 @@
     <p class="tagline">The seperation of sister chromatids.</p>
     <p class="information">
       Anaphase is the phase where the previously packed chromosomes are now going to be seperated.
-      It makes use of the connected microtubules to push poles apart which makes the cell visibly longer.
-      As the poles are being pushed apart,
-      <router-link to="/prophase">kinetochore microtubules</router-link> pull the chromosomes toward the poles,
-      which effectively seperates the chromosomes into <strong>individual chromosomes</strong>. This process is called
-      <strong>disjunction</strong>. All of these movements uses <strong>motor proteins</strong> in order to
-      move and seperate chromosomes.
+      It makes use of the microtubules who are connected to each other to push the poles apart. This
+      makes the cell look visibly longer than usual. Both microtubules and kinetochore microtubules
+      use <strong>motor proteins</strong> in order to move and seperate chromosomes.
+    </p>
+
+    <h2 class="sub-topic">
+      <strong>Disjunction</strong>
+    </h2>
+
+    <p class="information">
+      As the poles are being pushed apart by microtubules,
+      <router-link to="/prophase">kinetochore microtubules</router-link> pull the chromosomes toward the poles
+      that are being pushed away, which effectively seperates the chromosomes into
+      <strong>individual chromosomes</strong>. This process is called
+      <strong>disjunction</strong>.
+    </p>
+
+    <v-img
+      class="img"
+      src="https://cdn.kastatic.org/ka-perseus-images/aa3eddb779598a813e45ad6fd780cb82655758ed.png"
+    />
+
+    <p class="img-caption">Role of microtubules and kinetochore microtubules during anaphase</p>
+
+    <h2 class="sub-topic">
+      <strong>Nondisjunction</strong>
+    </h2>
+
+    <p class="information">
+      Nondisjunction is the term that is used when
+      <strong>disjunction fails</strong>. This happens because the chromosomes
+      fail to seperate from each other. The result of nondisjunction is there will be an
+      <strong>uneven number of chromosomes on opposing poles</strong>.
+      Nondisjunction leads to mutations, and if it occurs in meiosis can lead
+      to down syndrome, turner syndrome and etc in the offspring.
+    </p>
+
+    <v-img
+      class="img"
+      width="30%"
+      src="https://study.com/cimages/multimages/16/nondisjunction.png"
+    />
+
+    <p class="img-caption">
+      Visualization of uneven amount of chromosomes in the daughter cell.
+      <br>
+      First cell contains 5 chromosomes while the second one contains only 3.
     </p>
   </div>
 </template>
--- src/views/Telophase.vue
+++ src/views/Telophase.vue
@@ -1,14 +1,54 @@
 <template>
-  
+  <div class="content">
+    <h1 class="topic">
+      <strong>Telophase</strong>
+    </h1>
+
+    <p class="tagline">The last phase of cell division.</p>
+
+    <p class="information">
+      As the cell approaches to the final stage of cell division, the cell will start
+      to undo the changes and break down structures that were required for cell division.
+      The nuclear envelope reappears and the mitotic spindle starts to break down. Chromosomes
+      also start to unpack from their condensed form that was established in
+      <router-link to="/prophase">prophase</router-link>.
+    </p>
+
+    <h2 class="sub-topic">
+      <strong>Cytokinesis</strong>
+    </h2>
+
+    <p class="information">
+      Cytokinesis is when cytoplasm starts to divide in order to create a 2 new daughter cell. This process
+      does not need to wait till telophase starts, it can start during anaphase and finish after
+      telophase has completed.
+      <br>
+      <br>In animal cells, cytokinesis creates a crease on the cleavage line. The cleavage line is determined by
+      the <strong>contractile ring</strong>, which is made from a protein called actin. This determines where the
+      cell will contract and be pinched to seperate the daughter cells from the parent cell. This process will
+      create a crease called the <strong>clevage furrow</strong>.
+
+      <br>
+      <br>However in plant cells, the pinching between the daughter and the parent cell does not work, as
+      plants have a cell wall which is much stiffer than a cell membrane. In order for plants to
+      seperate from its daughter cells, they create a
+      <strong>cell plate</strong> which is basically
+      a wall that seperates the parent and daughter cell and eventually completely unattaching the
+      daughter cells from the parent.
+    </p>
+
+    <v-img
+      class="img"
+      src="https://cdn.kastatic.org/ka-perseus-images/f099142d1f9c234ff397699b9085a84f678116bc.png"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class Telophase extends Vue {
-
-}
+export default class Telophase extends Vue {}
 </script>
 
 <style lang="scss" scoped>
@@ -16,7 +56,7 @@ export default class Telophase extends Vue {
 
 a,
 strong {
-  color: #9C27B0;
+  color: #9c27b0;
 }
 </style>
 
